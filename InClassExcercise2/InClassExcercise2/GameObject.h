d7f1aa1bf53c1ce87bfb08c3ba0d4cd58d954b7b
CraigHolder <craig.holder@ontariotechu.net>
2020-10-02 14:45:52 -0400

    Init

---
+++ InClassExcercise2/InClassExcercise2/GameObject.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef __GAME_OBJECT__
+#define __GAME_OBJECT__
+
+#include "Vector2D.h"\
+
+
+class GameObject
+{
+	public;
+		GameObject();
+
+		int GetID() const;
+		void SetID(int id = 0);
+		Vector2D GetPosition() const;
+		void SetPosition()float x - 0.0f, 
+};
+
